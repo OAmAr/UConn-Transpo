5709cfcac258ef466f5dc3e4c0339978742fdb78
Cameron Morris <cameron.morris@uconn.edu>
2017-02-21 12:22:44 -0500

    test jenkins mailing list

---
+++ server/test_AlternativeFacts.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestAlternativeFacts(unittest.TestCase):
+    def test_SeanSpicer(self):
+        self.assertEqual(False, True)
+
+if __name__ == '__main__':
+    unittest.main()
