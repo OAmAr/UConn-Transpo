94da57ac43e5f165191be1bc2bee7366d2657526
Cameron Morris <cameron.morris@uconn.edu>
2017-02-21 13:04:48 -0500

    remove test test

--- server/test_AlternativeFacts.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestAlternativeFacts(unittest.TestCase):
-    def test_SeanSpicer(self):
-        self.assertEqual(False, True)
-
-if __name__ == '__main__':
-    unittest.main()
