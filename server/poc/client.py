45dbf8fee143bda897f591e9b6f1d6e0897dd8d2
Cameron Morris <cameron.morris@uconn.edu>
2017-04-02 15:14:42 -0400

    add udp-bus-location-responder to server

---
+++ server/poc/client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+import struct
+
+HOST, PORT = "localhost", 6269
+data = 40
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+for i in range(1):
+    sock.sendto(struct.pack('!i', data), (HOST, PORT))
+    data+=1
+received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
