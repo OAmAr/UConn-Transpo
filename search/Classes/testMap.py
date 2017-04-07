b1b504a1595f9bd526251607b8469f021c77d44c
Omar Amer <omar.amer@uconn.edu>
2017-04-07 12:30:09 -0400

    TABULAR HELL IS REAL AND IT IS HERE BUT I THE MESSIAH OF REGEX HAVE DONE AWAY WITH THE SINS OF PARAM

--- search/Classes/testMap.py
+++ search/Classes/testMap.py
@@ -5,15 +5,15 @@ except ImportError:
     from search.Classes.Map import Map
 
 class testLoc(unittest.TestCase):
-	def testNonrealStop(self):
-		m = Map()
-		self.assertEquals(myMap.getTime("MSB","Non-real location"),None) # maybe this should raise an error?
+    def testNonrealStop(self):
+    	myMap = Map()
+    	self.assertEquals(myMap.getTime("MSB","Non-real location"),None) # maybe this should raise an error?
 
 
-	def testBasic(self):
-		L = Location(8,7)
-		self.assertEqual(L.getLocation(), [8,7])
+    def testBasic(self):
+    	L = Location(8,7)
+    	self.assertEqual(L.getLocation(), [8,7])
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
