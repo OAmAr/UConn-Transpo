3f647965ea1aeb36102cf55eec112eaca691f7e4
Cameron Morris <cameron.morris@uconn.edu>
2017-04-22 21:49:01 -0400

    add class to parse datagram data

---
+++ UConnBusApp/app/src/main/java/com/example/vedant/uconnbusapp/BusLocationDatagram.java
@@ -0,0 +1,28 @@
+package com.example.vedant.uconnbusapp;
+
+/** Parse the information out of a Bus Location Datagram.
+ * @author Cameron Morris
+ */
+
+public class BusLocationDatagram {
+    public byte flags;
+    public byte seconds;
+    public short heading;
+    public short RouteID;
+    public short VehicleID;
+    public float latitude;
+    public float longitude;
+    public float groundSpeed;
+
+    BusLocationDatagram(byte[] buffer) {
+        java.nio.ByteBuffer wrapped = java.nio.ByteBuffer.wrap(buffer);
+        flags = wrapped.get();
+        seconds = wrapped.get();
+        heading = wrapped.getShort();
+        RouteID = wrapped.getShort();
+        VehicleID = wrapped.getShort();
+        latitude = wrapped.getFloat();
+        longitude = wrapped.getFloat();
+        groundSpeed = wrapped.getFloat();
+    }   
+}
