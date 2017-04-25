3b54bb164c8d5d1f8281bcecc1ee59a3664642c4
Cameron Morris <cameron.morris@uconn.edu>
2017-04-25 11:33:43 -0400

    fixing broken things

--- UConnBusApp/app/src/main/java/com/example/vedant/uconnbusapp/MapsActivity.java
+++ UConnBusApp/app/src/main/java/com/example/vedant/uconnbusapp/MapsActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
